8cdd9fc064177a711ec65f0e10ebda4c9ab14e01
hamza123-lang <63500865+hamza123-lang@users.noreply.github.com>
2023-08-19 12:42:51 +0000

    Add files via upload

---
+++ stochastic population growth.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 19 11:47:12 2023
+
+@author: hamza
+"""
+
+import numpy as np 
+
+"""n is the population at t0 
+modelling the growth of a population 
+g number of generation(iterations) 
+l is lumbda for poisson         """
+def populationgrowth(n,g,l):
+    rng = np.random.default_rng()
+    
+    for i in range (g):
+        m= rng.poisson(lam=l, size=n)
+        n=np.sum(m)
+        print(n)
+populationgrowth(10, 10, 2)        
